8aa912dba0f7a1fe8fe1f39cb1329e3bfec83666
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-02-08 07:56:30 -0800

    Separate process "server" and HTTP "client"

--- demo/demo_cli.py
+++ demo/demo_cli.py
@@ -1,13 +1,29 @@
 import subprocess
 import sys
 from pathlib import Path
+import time
+import signal
 
 demo_base_path = Path(__file__).parent
 
 client_path = demo_base_path / "client.html"
 server_path = demo_base_path / "server.py"
-parallel_script_path = demo_base_path / "parallel_script.py"
+parallel_server_path = demo_base_path / "parallel" / "server.py"
+parallel_client_path = demo_base_path / "parallel" / "client.py"
 
+# List to keep track of subprocesses
+subprocesses = []
+
+def close_subprocesses():
+    for process in subprocesses:
+        process.terminate()  # Send SIGTERM to subprocess
+    sys.exit(0)
+
+def signal_handler(signal, frame):
+    print('Interrupt signal received. Shutting down subprocesses...')
+    close_subprocesses()
+
+signal.signal(signal.SIGINT, signal_handler)
 
 def main():
     if len(sys.argv) <= 1:
@@ -25,21 +41,54 @@ def main():
     flags = dict(
         client=not server_flag or both_flags,
         server=not client_flag or both_flags,
+        both=(client_flag and server_flag) or (not client_flag and not server_flag)
     )
 
     if command == "demo":
         if flags["client"]:
-            subprocess.run(["open", client_path])
+            subprocesses.append(subprocess.Popen(["open", client_path]))
 
         if flags["server"]:
-            subprocess.run(["python", server_path])
+            subprocesses.append(subprocess.Popen(["python", server_path]))
 
-    elif command == "parallel":
-        subprocess.run(["python", parallel_script_path])
+    elif command == "parallel-demo":
+        HOST = "localhost"
+        PORT = 8000
+
+        if "--port" in flags_list:
+            port_index = flags_list.index("--port")
+            PORT = flags_list[port_index + 1]
+        elif both_flags:
+            print(f"Port not provided. Using default port ({PORT})")
+
+        if "--host" in flags_list:
+            host_index = flags_list.index("--host")
+            HOST = flags_list[host_index + 1]
+        elif both_flags:
+            print(f"Host not provided. Using default host ({HOST})")
+
+        if flags["client"]:
+            client_args = ["python", parallel_client_path]
+            if flags["both"]:
+                client_args += ["--port", str(PORT), "--host", HOST]
+            subprocesses.append(subprocess.Popen(client_args))
+            if flags["both"]:
+                time.sleep(1)  # Ensure server starts before client connects
+
+        if flags["server"]:
+            server_args = ["python", parallel_server_path]
+            if flags["both"]:
+                server_args += ["--port",  str(PORT), "--host", HOST]
+            subprocess.run(server_args)
 
     else:
         print(f"{command} is an invalid command.")
 
-
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print('\n\nInterrupt signal received. Shutting down subprocesses...')
+    finally:
+        close_subprocesses()
+
---
+++ demo/parallel/server.py
@@ -0,0 +1,85 @@
+"""Demo of parallel tqdm."""
+
+# HTTP server addition
+import http.server
+import json
+import socket
+import socketserver
+import threading
+import time
+import uuid
+from concurrent.futures import ProcessPoolExecutor
+from typing import List, Tuple
+
+import requests
+import sys
+
+from tqdm_publisher import TQDMPublisher
+
+def _run_sleep_in_subprocess(args: Tuple[int, int]):
+    """The operation to run on each subprocess."""
+    repeat = args[0]
+    iteration_index = args[1]
+    id = args[2]
+    url = args[3]
+
+    iterable = range(repeat)
+    sub_progress_bar = TQDMPublisher(
+        iterable=iterable,
+        position=iteration_index + 1,
+        desc=f"Progress on iteration {iteration_index} ({id})",
+        leave=False,
+    )
+
+    if url:
+
+        def to_main_process(n: int, total: int, **kwargs):
+            
+            json_data = json.dumps(dict(
+                id=str(id),
+                data=dict(
+                    n = n,
+                    total = total
+                )
+            ))
+
+            requests.post(url, data=json_data, headers={"Content-Type": "application/json"})
+
+        sub_progress_bar.subscribe(to_main_process)
+
+    for _ in sub_progress_bar:
+        time.sleep(0.5)
+
+
+
+if __name__ == "__main__":
+    number_of_jobs = 3
+    repeats = [4, 6, 8, 10, 4, 8, 20, 10, 5, 12, 5, 4, 5, 5, 5]
+
+    flags_list = sys.argv[1:]
+
+    port_flag = "--port" in flags_list
+    host_flag = "--host" in flags_list
+
+    if port_flag:
+        port_index = flags_list.index("--port")
+        PORT = flags_list[port_index + 1]
+    
+    if (host_flag):
+        host_index = flags_list.index("--host")
+        HOST = flags_list[host_index + 1]
+    else:
+        HOST = "localhost"
+
+
+    URL = f"http://{HOST}:{PORT}" if port_flag else None
+
+    # Start the parallel jobs
+    with ProcessPoolExecutor(max_workers=number_of_jobs) as executor:
+
+        job_map = executor.map(
+            _run_sleep_in_subprocess,
+            [(repeat, iteration_index, uuid.uuid4(), URL) for iteration_index, repeat in enumerate(repeats)],
+        )
+
+        [_ for _ in job_map]  # perform iteration to deploy jobs
