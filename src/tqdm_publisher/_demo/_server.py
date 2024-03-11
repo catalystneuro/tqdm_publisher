388d4be4564ddc36f7c93f28f811515605f40419
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-03-11 14:52:17 -0400

    Improve non-parallel demo (#40)
    * fixing for M1 Mac
    
    * Fix name of file targeted
    
    * Simplify demo structure (#39)
    
    * Simplify synchronous demo
    
    * Update docs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    
    * suggestions for readability
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update descriptions and types
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update _server.py
    
    * Fix demo
    
    * Document the frontend
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update src/tqdm_publisher/_demo/_client.js
    
    * Functional websocket declaration (#42)
    
    * Update _server.py
    
    * more keyword arguments; break up docstring; remove unused capture
    
    * more keyword arguments
    
    ---------
    
    Co-authored-by: Garrett Michael Flynn <garrettmflynn@gmail.com>
    
    ---------
    
    Co-authored-by: CodyCBakerPhD <codycbakerphd@gmail.com>
    Co-authored-by: Garrett Michael Flynn <garrettmflynn@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/tqdm_publisher/_demo/_server.py
@@ -0,0 +1,74 @@
+import asyncio
+import json
+import threading
+import time
+
+import websockets
+
+import tqdm_publisher
+
+
+def start_progress_bar(*, progress_bar_id: str, client_callback: callable) -> None:
+    """
+    Emulate running the specified number of tasks by sleeping the specified amount of time on each iteration.
+
+    Defaults are chosen for a deterministic and regular update period of one second for a total time of one minute.
+    """
+    all_task_durations_in_seconds = [1.0 for _ in range(60)]  # One minute at one second per update
+    progress_bar = tqdm_publisher.TQDMPublisher(iterable=all_task_durations_in_seconds)
+
+    def run_function_on_progress_update(format_dict: dict) -> None:
+        """
+        This is the injected callback that will be run on each update of the TQDM object.
+
+        Its first and only positional argument must be the `format_dict` of the TQDM instance. Additional customization
+        on outside parameters must be achieved by defining those fields at an outer scope and defining this
+        server-specific callback inside the local scope.
+
+        In this demo, we will execute the `client_callback` whose protocol is known only to the WebSocketHandler.
+        """
+        client_callback(progress_bar_id=progress_bar_id, format_dict=format_dict)
+
+    progress_bar.subscribe(callback=run_function_on_progress_update)
+
+    for task_duration in progress_bar:
+        time.sleep(task_duration)
+
+
+async def handler(websocket: websockets.WebSocketServerProtocol) -> None:
+    """Handle messages from the client and manage the client connections."""
+
+    def forward_progress_to_client(*, progress_bar_id: str, format_dict: dict) -> None:
+        """
+        This is the function that will run on every update of the TQDM object.
+
+        It will forward the progress to the client.
+        """
+        asyncio.run(
+            websocket.send(message=json.dumps(obj=dict(progress_bar_id=progress_bar_id, format_dict=format_dict)))
+        )
+
+    # Wait for messages from the client
+    async for message in websocket:
+        message_from_client = json.loads(message)
+
+        if message_from_client["command"] == "start":
+            thread = threading.Thread(
+                target=start_progress_bar,
+                kwargs=dict(
+                    progress_bar_id=message_from_client["progress_bar_id"],
+                    client_callback=forward_progress_to_client,
+                ),
+            )
+            thread.start()
+
+
+async def spawn_server() -> None:
+    """Spawn the server asynchronously."""
+    async with websockets.serve(ws_handler=handler, host="", port=8000):
+        await asyncio.Future()
+
+
+def run_demo() -> None:
+    """Trigger the execution of the asynchronous spawn."""
+    asyncio.run(spawn_server())
