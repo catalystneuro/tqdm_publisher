8b515720f9f07291ad9afce558d5e6796b0e45d1
codycbakerphd <cbaker9@nd.edu>
2024-03-11 14:18:11 -0400

    more keyword arguments; break up docstring; remove unused capture

--- src/tqdm_publisher/_demo/_server.py
+++ src/tqdm_publisher/_demo/_server.py
@@ -2,12 +2,10 @@ import asyncio
 import json
 import threading
 import time
-from typing import Any, Dict, List
-from uuid import uuid4
 
 import websockets
 
-from tqdm_publisher import TQDMPublisher
+import tqdm_publisher
 
 
 def start_progress_bar(*, progress_bar_id: str, client_callback: callable) -> None:
@@ -17,7 +15,7 @@ def start_progress_bar(*, progress_bar_id: str, client_callback: callable) -> No
     Defaults are chosen for a deterministic and regular update period of one second for a total time of one minute.
     """
     all_task_durations_in_seconds = [1.0 for _ in range(60)]  # One minute at one second per update
-    progress_bar = TQDMPublisher(iterable=all_task_durations_in_seconds)
+    progress_bar = tqdm_publisher.TQDMPublisher(iterable=all_task_durations_in_seconds)
 
     def run_function_on_progress_update(format_dict: dict) -> None:
         """
@@ -41,33 +39,31 @@ async def handler(websocket: websockets.WebSocketServerProtocol) -> None:
     """Handle messages from the client and manage the client connections."""
 
     def forward_progress_to_client(*, progress_bar_id: str, format_dict: dict) -> None:
-        """This is the function that will run on every update of the TQDM object. It will forward the progress to the client."""
+        """
+        This is the function that will run on every update of the TQDM object.
+
+        It will forward the progress to the client.
+        """
         asyncio.run(websocket.send(json.dumps(obj=dict(progress_bar_id=progress_bar_id, format_dict=format_dict))))
 
     # Wait for messages from the client
-    try:
-        async for message in websocket:
-            message_from_client = json.loads(message)
+    async for message in websocket:
+        message_from_client = json.loads(message)
 
-            if message_from_client["command"] == "start":
-                thread = threading.Thread(
-                    target=start_progress_bar,
-                    kwargs=dict(
-                        progress_bar_id=message_from_client["progress_bar_id"],
-                        client_callback=forward_progress_to_client
-                    ),
-                )
-                thread.start()
-
-    # Catch the closing of the connection
-    finally:
-        pass
-        
+        if message_from_client["command"] == "start":
+            thread = threading.Thread(
+                target=start_progress_bar,
+                kwargs=dict(
+                    progress_bar_id=message_from_client["progress_bar_id"],
+                    client_callback=forward_progress_to_client,
+                ),
+            )
+            thread.start()
 
 
 async def spawn_server() -> None:
     """Spawn the server asynchronously."""
-    async with websockets.serve(handler, "", 8000):
+    async with websockets.serve(ws_handler=handler, host="", port=8000):
         await asyncio.Future()
 
 
