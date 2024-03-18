a9a2906b11af1bdceab4ae54dec14e3279cc2962
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2024-03-18 15:26:52 -0400

    unpack anonymous callable

--- src/tqdm_publisher/_subscriber.py
+++
@@ -1,7 +0,0 @@
-from ._publisher import TQDMPublisher
-
-
-class TQDMProgressSubscriber(TQDMPublisher):
-    def __init__(self, iterable, on_progress_update: callable, **tqdm_kwargs):
-        super().__init__(iterable, **tqdm_kwargs)
-        self.subscribe(lambda format_dict: on_progress_update(dict(progress_bar_id=self.id, format_dict=format_dict)))
