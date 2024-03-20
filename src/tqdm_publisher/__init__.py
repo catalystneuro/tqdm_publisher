aeae8f8c74ac8854a00ab3610373137799c52974
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-03-20 11:31:20 -0700

    Create a queue handler for cases where a single callback is used across all messages on all progress bars

--- src/tqdm_publisher/__init__.py
+++ src/tqdm_publisher/__init__.py
@@ -1,4 +1,5 @@
 from ._publisher import TQDMPublisher
-from ._subscriber import TQDMProgressSubscriber
+from ._progress_subscriber import TQDMProgressSubscriber
+from ._progress_handler import TQDMProgressHandler
 
-__all__ = ["TQDMPublisher", "TQDMProgressSubscriber"]
+__all__ = ["TQDMPublisher", "TQDMProgressSubscriber", "TQDMProgressHandler"]
--- src/tqdm_publisher/_progress_subscriber.py
+++ src/tqdm_publisher/_progress_subscriber.py
@@ -1,14 +1,6 @@
 from ._publisher import TQDMPublisher
 
-
 class TQDMProgressSubscriber(TQDMPublisher):
-    def __init__(self, iterable, *, announcer: "?", request_id: str, message: dict, **tqdm_kwargs):
+    def __init__(self, iterable, on_progress_update: callable, **tqdm_kwargs):
         super().__init__(iterable, **tqdm_kwargs)
-
-        def on_progress_update(format_dict) -> None:
-            """
-            Describe what this announcer is all about...
-            """
-            announcer.announce(dict(request_id=request_id, **message))
-
-        self.subscribe(callback=on_progress_update)
+        self.subscribe(lambda format_dict: on_progress_update(dict(progress_bar_id=self.id, format_dict=format_dict)))
