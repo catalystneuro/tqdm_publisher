1272fc16615454fff1d51a97b9dd80da85f9bbe5
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-04-25 14:44:16 -0700

    100% coverage

--- src/tqdm_publisher/testing.py
+++ src/tqdm_publisher/testing.py
@@ -6,7 +6,7 @@ async def sleep_func(sleep_duration: float = 1) -> float:
     await asyncio.sleep(delay=sleep_duration)
 
 
-def create_tasks():
+def create_tasks(number_of_tasks: int = 10**5):
     number_of_tasks = 10**5
     sleep_durations = [random.uniform(0, 5.0) for _ in range(number_of_tasks)]
 
